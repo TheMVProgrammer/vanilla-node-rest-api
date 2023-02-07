ec4ba012f53013704a281f8174cff62977968a2b
TheMVProgrammer <franklin0415@hotmail.com>
2023-02-07 17:29:34 -0400

    adding some functionalities

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
-const products = require('./data/products.json');
+const { getProducts, getProduct, createProduct } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(products)); 
+        getProducts(req, res);
+    }
+    else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        getProduct(req, res, id);
+    }
+    else if (req.url === '/api/products' && req.method === 'POST') {
+        createProduct(req, res);
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
